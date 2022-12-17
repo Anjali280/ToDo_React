5a45410499d16bfbbf0b32b5629659bcc1c13bbb
Anjali280 <mondalanjali0707@gmail.com>
2022-12-17 13:43:05 +0530

    ToDo List Done

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,19 +1,33 @@
 import React from "react";
 import "./Todo.css";
+import TodoList from "./TodoList";
 
 export default function Todo() {
   const [inputList, setInputList] = React.useState("");
   const [Items, setItems] = React.useState([]);
 
+  // for getting the input value from the target.....
   const itemEvent = (event) => {
     setInputList(event.target.value);
   };
 
+  // After getting the input vaue from the target , put it in the array....
   const itemsDisplay = () => {
     setItems((oldItems) => {
       return [...oldItems, inputList];
     });
     setInputList("");
+  };
+
+  // for deleting the items
+  // index = its the index no of the all the items in the olditems array
+  // id = this is the particulary selected item for deleting
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrayElem, index) => {
+        return id !== index;
+      });
+    });
   };
   return (
     <div>
@@ -31,9 +45,16 @@ export default function Todo() {
           <button onClick={itemsDisplay}> + </button>
 
           <ol>
-            {/* <li>{inputList}</li> */}
-            {Items.map((itemval) => {
-              return <li> {itemval} </li>;
+            {Items.map((itemval, index) => {
+              // key={index} each child in a list should have a unique key prop......
+              return (
+                <TodoList
+                  text={itemval} // for displaying data
+                  key={index}
+                  id={index}
+                  onSelect={deleteItems} // for deleting items
+                />
+              );
             })}
           </ol>
         </div>
